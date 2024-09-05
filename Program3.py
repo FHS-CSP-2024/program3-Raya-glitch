3bdddefb68cca9b99a8319b004ffb7d7670f8f7f
Raya-glitch <yaram.gamer@gmail.com>
2024-09-05 17:54:55 +0000

    I am not done

--- Program3.py
+++ Program3.py
@@ -2,6 +2,39 @@
 #Fix the following code so that its output matches
 # Sample output:
 # --------------------------------------------------
+name = "Bob"
+Name = "Chad"
+print(Name + name)
+
+myString = "something"
+myInt = 100
+myFloat = 2.5
+
+#casting example
+print(myInt/2)
+myNewInt = "100"
+print(int(myNewInt)/2)
+
+num1 = input("Please enter a number")
+
+print(int(num1) + int(num1)) 
+
+print("My int = " + str(myInt))   
+      
+thing = 500/6 
+print(thing)
+print("My result is " + str(thing))
+print("My result is ", thing)
+print(f"My result is {thing}") #fstring - {} are for variables   
+print("This is\n Second line\nThird line")
+print("List header")
+print("\t* Item1")
+print("\t* Item2")
+print("\t* Item3\n\t* Item4\n\t* Item5")
+#File path example
+#C:\Users\myname\Documents\Onenote Notebooks
+print("C:\\User\\mrJohnson\\Document\\hhdhwehudwuehf...")
+         
 # my name is Tim Tester, I am 20 years old
 # 
 # my skills are
@@ -22,12 +55,12 @@ level3 = "semiprofessional"
 lower = 2000
 upper = 3000
 
-print("my name is ", name, " , I am ", age, "years old")
+print("my name is",name, "I am " , age, "years old")
 print("my skills are")
-print("- ", skill1, " (", level1, ")")
-print("- ", skill2, " (", level2, ")")
-print("- ", skill3, " (", level3, " )")
-print("I am looking for a job with a salary of", lower, "-", upper, "dollars per month")
+print("- ", skill1, "(",level1,")")
+print("- ", skill2, "(",level2,")")
+print("- ", skill3, "(",level3,")")
+print("I am looking for a job with a salary of", lower, "-" ,upper, "dollars per month")
 
 
 
@@ -50,4 +83,8 @@ print("I am looking for a job with a salary of", lower, "-", upper, "dollars per
 
 x = input("X val: ")
 y = input("Y val: ")
+
+
+
+
 
